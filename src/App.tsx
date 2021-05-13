dd3fbb5307547fbf39a3669a19d20a6b7c74d8e5
Mihail Preis <mihail.preis@waveaccess.ru>
2021-05-13 12:12:31 +0500

    Add dev-menu concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './ui/home/HomeScreen';
 import CatViewer from './ui/cat/CatViewer';
 import UsersScreen from './ui/user/UsersScreen';
 import UserDetails from './ui/user/UserDetails';
+import RNShake from 'react-native-shake';
+import DevMenuScreen from './ui/devmenu/DevMenuScreen';
 
-const Stack = createStackNavigator();
+const RootStack = createStackNavigator();
+const MainStack = createStackNavigator();
+
+function Main({ navigation, route }) {
+
+  useEffect(() => {
+    let shakeHandler = () => navigation.navigate('DevMenu');
+    RNShake.addEventListener('ShakeEvent', shakeHandler);
+    return () => RNShake.removeEventListener('ShakeEvent', shakeHandler);
+  }, [])
+
+  return (
+    <MainStack.Navigator initialRouteName="Home">
+      <MainStack.Screen name="Home" component={HomeScreen} options={{ title: '🐄💨' }} />
+      <MainStack.Screen name="CatViewer" component={CatViewer} options={{ title: '🐈🐈‍⬛🐈' }} />
+      <MainStack.Screen name="UsersViewer" component={UsersScreen} options={{ title: 'Users' }} />
+      <MainStack.Screen name="UserDetails" component={UserDetails} options={{ title: 'John Doe' }} />
+    </MainStack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: '🐄💨' }} />
-        <Stack.Screen name="CatViewer" component={CatViewer} options={{ title: '🐈🐈‍⬛🐈' }} />
-        <Stack.Screen name="UsersViewer" component={UsersScreen} options={{ title: 'Users' }} />
-        <Stack.Screen name="UserDetails" component={UserDetails} options={{ title: 'John Doe' }} />
-      </Stack.Navigator>
+      <RootStack.Navigator mode="modal" initialRouteName="Main">
+        <RootStack.Screen name="Main" component={Main} options={{ headerShown: false }} />
+        <RootStack.Screen name="DevMenu" component={DevMenuScreen} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ src/ui/devmenu/DevMenuScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image, Button, Alert, ScrollView, TouchableOpacity } from 'react-native';
+
+export default function DevMenuScreen({ navigation, route }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Hi!</Text>
+        </View>
+    );
+}
