08e557815905ae30dfb73cafbecd9faf1f7e0a2d
Mihail Preis <mihail.preis@waveaccess.ru>
2021-05-18 17:32:12 +0500

    Update UserCard

--- src/ui/user/UserCard.tsx
+++ src/ui/user/UserCard.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, Image, FlatList, TouchableHighlight, SafeAreaView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import UserModel from './UserModel';
 
 const styles = StyleSheet.create({
@@ -7,14 +7,27 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         backgroundColor: '#FFF1FF',
         margin: 5,
-        padding: 10,
-        paddingTop: 15
+        padding: 15,
+        paddingTop: 15,
+        flexDirection: 'row'
     },
     avatar: {
         height: 52,
         width: 50,
         borderRadius: 25,
         borderWidth: 2
+    },
+    inner_container: {
+        flexDirection: 'column',
+        alignSelf: 'center',
+        paddingStart: 15,
+    },
+    name_title: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    val_title: {
+        fontSize: 14
     }
 });
 
@@ -24,14 +37,18 @@ export interface UserCardProps {
 
 export default function UserCard(props: UserCardProps) {
     const avatarBorder = props.model.gender == 'male' ? '#31ebe4' : '#fa41e7';
+    const name = [props.model.name.first, props.model.name.last]
+        .filter((i) => i.length > 0)
+        .join(' ');
     return (
         <View style={styles.container}>
-            <View style={{ flexDirection: 'row' }}>
-                <Image style={{ ...styles.avatar, borderColor: avatarBorder }} source={{ uri: props.model.picture.medium }}></Image>
-                <View style={{ flexDirection: 'column', paddingStart: 10 }}>
-                    <Text style={{ fontWeight: 'bold' }}>{[props.model.name.first, props.model.name.last].filter((i) => i.length > 0).join(' ')}</Text>
-                    <Text style={{ fontSize: 12 }}>{props.model.email}</Text>
-                </View>
+            <Image
+                style={{ ...styles.avatar, borderColor: avatarBorder }}
+                source={{ uri: props.model.picture.medium }}
+            />
+            <View style={styles.inner_container}>
+                <Text style={styles.name_title}>{name}</Text>
+                <Text style={styles.val_title}>{props.model.email}</Text>
             </View>
         </View>
     );
